ed7504e3c8b2755764ea9d4f151532f41af3df33
Your Name <you@example.com>
2019-08-15 17:10:04 +0100

    code for server that returns query results finished

--- src/api/main_server.py
+++ src/api/main_server.py
@@ -3,12 +3,9 @@ from retrieval.retrieval_bert_index import RetrieveBertIndex
 import extract_requested_data_for_UI as Data4UI
 from flask import Flask, request
 
-
 app = Flask(__name__)
 
-
 # creating some global objects
-
 # Retrieve objects using the MeSH trained BERT encoder representations
 retr_bert_idx = RetrieveBertIndex()
 # Retrieve objects using the inverted index based on words occurence
@@ -20,18 +17,30 @@ def retrieve_bert_based_results():
 	topk = int(request.form["topk"])
 	# retrieve the query results
 	res_bert_idx = retr_bert_idx.retrieve_query_results(query, topk=topk//2)
-	output = Data4UI.extract_data(query)
+	output = Data4UI.extract_data(res_bert_idx)
 	return output
 
 @app.route('/query/inverted',  methods='POST'):
+def retrieve_inverted_index_based_results():
 	query = str(request.form["query"])
 	topk = int(request.form["topk"])
 	# retrieve the query results
 	res_inv_idx = retr_inv_idx.retrieve_query_results(query, topk=topk//2)
-	output = Data4UI.extract_data(query)
+	output = Data4UI.extract_data(res_inv_idx)
 	return output
 
 
+@app.route('/query/combined_results',  methods='POST')
+	query = str(request.form["query"])
+	topk = int(request.form["topk"])
+	# retrieve the query results from inverted index
+	res_inv_idx = retr_inv_idx.retrieve_query_results(query, topk=topk//2)
+	# retrieve the query results from bert index
+	res_bert_idx = retr_bert_idx.retrieve_query_results(query, topk=topk//2)
 
+	combined_query_results = res_inv_idx + res_bert_idx
+	output = Data4UI.extract_data(combined_query_results)
+
+	return output
 
 
